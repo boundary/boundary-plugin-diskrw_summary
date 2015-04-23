abb30e8f9d9b6db4aa1f6fbfd59569b9828e9850
TheBigHNF <henry@boundary.com>
2015-04-23 14:07:56 -0500

    Initial Version

---
+++ README.md
@@ -0,0 +1,37 @@
+# Boundary Disk Read/Write Summary Plugin
+
+This plugin scans one or more devices/drives for available disk space as a percentage.
+
+## Prerequisites
+
+### Supported OS
+
+|     OS    | Linux | Windows | SmartOS | OS X |
+|:----------|:-----:|:-------:|:-------:|:----:|
+| Supported |   v   |    v    |    v    |  v   |
+
+#### Requires Boundary Meter Versions V4.0 or later
+
+- To install new meter go to Settings->Installation or [see instructons|https://help.boundary.com/hc/en-us/sections/200634331-Installation]. 
+- To upgrade the meter to the latest version - [see instructons|https://help.boundary.com/hc/en-us/articles/201573102-Upgrading-the-Boundary-Meter].
+
+### Plugin Setup
+
+None
+
+#### Plugin Configuration Fields
+
+|Field Name|Description                                       |
+|:----------|:------------------------------------------------|
+|Disk Name  |The name of the disk to be appended to the hostname to display in the legend for the Disk Use Summary data."|
+|Mount Point|The mounted point to check for free space. (either this or the Mounted device need to be set for the plugin to function properly)|
+|Device     |The mounted device to check for free space. (either this or the Mount Point directory need to be set for the plugin to function properly)|
+
+### Metrics Collected
+
+#### For All Versions
+
+|Metric Name            |Description               |
+|:----------------------|:-------------------------|
+|Disk Read/Write Summary|The percentage of disk used for the given mounted disk|
+
---
+++ init.lua
@@ -0,0 +1,84 @@
+local json = require('json')
+local framework = require('framework.lua')
+framework.table()
+framework.util()
+framework.functional()
+local stringutil = framework.string
+
+local Plugin = framework.Plugin
+local NetDataSource = framework.NetDataSource
+local net = require('net')
+require('fun')(true) -- Shows a warn when overriding an existing function.
+
+local items = {}
+
+local params = framework.boundary.param
+params.name = 'Boundary Disk Use Summary'
+params.version = '2.0'
+
+items = params.items or items
+
+local meterDataSource = NetDataSource:new('127.0.0.1', '9192')
+
+function meterDataSource:onFetch(socket)
+	socket:write('{"jsonrpc":"2.0","method":"query_metric","id":1,"params":{"match":"system.disk"}}\n')
+end
+
+local meterPlugin = Plugin:new(params, meterDataSource)
+
+function meterPlugin:onParseValues(data)
+	
+  local result = {}
+  local parsed = json.parse(data)
+  if table.getn(parsed.result.query_metric) > 0 then
+    for i = 1, table.getn(parsed.result.query_metric), 3 do
+
+      local metric = {}
+      local typestart, typeend = string.find(parsed.result.query_metric[i], "system.disk.")
+      typestart = typeend+1
+      typeend = string.find(parsed.result.query_metric[i], "%.", typestart) or string.find(parsed.result.query_metric[i], "|", typestart)
+      local type = string.sub(parsed.result.query_metric[i], typestart, typeend-1)
+      if string.sub(parsed.result.query_metric[i], typeend+1) == "total" then
+        metric.metric = "TOTAL_DISK_"..string.upper(type)
+        metric.source = meterPlugin.source
+        metric.value = parsed.result.query_metric[i+1]
+        table.insert(result, metric)
+      else
+        local dirname = stringutil.urldecode(string.sub(parsed.result.query_metric[i], string.find(parsed.result.query_metric[i], "dir=")+4, string.find(parsed.result.query_metric[i], "&")-1))
+        local devname = stringutil.urldecode(string.sub(parsed.result.query_metric[i], string.find(parsed.result.query_metric[i], "dev=")+4, -1))
+        local sourcename=meterPlugin.source.."."
+        local capture_metric = 0
+
+        for _, item in ipairs(items) do
+          if item.dir  then
+            if (item.dir == dirname) then
+              if item.device then
+                if (item.device == devname) then
+                  capture_metric = 1
+                  sourcename = sourcename..(item.diskname or dirname.."."..devname)
+                end
+              else
+                capture_metric = 1
+                sourcename = sourcename..(item.diskname or dirname.."."..devname)
+              end
+            end
+          elseif item.device and (item.device == devname) then
+            capture_metric = 1
+            sourcename = sourcename..(item.diskname or dirname.."."..devname)
+          end
+        end
+        if capture_metric == 1 then
+          metric.metric = "DISK_"..string.upper(type).."_SUMMARY"
+          metric.source = '"'..sourcename..'"'
+          metric.value = parsed.result.query_metric[i+1]
+          table.insert(result, metric)
+        end
+      end
+    end
+  end
+
+  return result	
+
+end
+
+meterPlugin:run()
